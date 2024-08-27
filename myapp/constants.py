f8a61eb8e01ff4534686bb79b6d869ded893436f
shazzy <ssheshant@gmail.com>
2024-08-27 17:38:11 +0530

    Update constants.py
    removed passwords

--- myapp/constants.py
+++ myapp/constants.py
@@ -1,10 +1,5 @@
 import os
 
-# HOST = os.environ.get("ES_HOST_NAME", "https://search-elasticsearch-demo-xqf7vdiyrpc5ipbtsiu7hf27qe.ap-south-1.es.amazonaws.com")
-# USERNAME = os.environ.get("ES_USERNAME", "elasticsearch_admin")
-# PASSWORD = os.environ.get("ES_PASSWORD", "C#a7@DCTY94IeGmF")
-
-
-HOST = os.environ.get("ES_HOST_NAME", "https://168738afe2aa4aebbf3060af19ce5638.ap-south-1.aws.elastic-cloud.com:443")
-USERNAME = os.environ.get("ES_USERNAME", "elastic")
-PASSWORD = os.environ.get("ES_PASSWORD", "Bk4hoL9eX8SPuMhvt86gd9pd")
+HOST = os.environ.get("ES_HOST_NAME")
+USERNAME = os.environ.get("ES_USERNAME")
+PASSWORD = os.environ.get("ES_PASSWORD")
